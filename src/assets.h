510de21a2f1f76984183698d73391d8059552e5a
Aman Gupta <aman@tmm1.net>
2020-01-16 21:41:24 -0800

    split out assets into separate source file

---
+++ src/assets.h
@@ -0,0 +1,13 @@
+#ifndef ASSETS_H
+#define ASSETS_H
+
+extern const char index_html[];
+extern const char rsa_js[];
+extern const char style_css[];
+extern const char rng_js[];
+extern const char jsbn_2_js[];
+extern const char jsbn_1_js[];
+extern const char script_js[];
+extern const char prng4_js[];
+
+#endif
