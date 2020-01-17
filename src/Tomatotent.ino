79079671893215190905e73ace8fb2b435612d10
Aman Gupta <aman@tmm1.net>
2020-01-16 21:56:28 -0800

    move license to separate file

--- src/Tomatotent.ino
+++ src/Tomatotent.ino
@@ -27,22 +27,6 @@ unsigned long lastTime2 = 0;
 bool dimmerButtonPressed = false;
 bool dimmerBtnVal;
 
-
-/***************************************************
-  This is our GFX example for the Adafruit ILI9341 Breakout and Shield
-  ----> http://www.adafruit.com/products/1651
-
-  Check out the links above for our tutorials and wiring diagrams
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional)
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, D6);
 DFRobot_SHT20 sht20;
 Button buttons[6];
