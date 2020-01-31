83af6c6532feafbae54966972daec263707498d2
Aman Gupta <aman@tmm1.net>
2020-01-30 18:21:17 -0800

    redesign cancel screen to allow day counter changes

--- src/screens/cancel.cpp
+++ src/screens/cancel.cpp
@@ -10,26 +10,24 @@ void CancelScreen::render()
 {
     tft.fillScreen(ILI9341_BLACK);
 
-    tft.setCursor(20, 70);
-    tft.setTextColor(ILI9341_WHITE);
-    tft.setTextSize(2);
-    tft.print("Your grow is " + String(tent.state.getDayCount() - 1) + " days old.");
+    drawDay();
 
-    tft.setCursor(20, 100);
-    tft.print("Most Cannabis is ready");
+    buttons.push_back(Button("dayUpBtn", 50, 35, 40, 40, "", 0, 0));
+    buttons.push_back(Button("dayDownBtn", 50, 135, 40, 40, "", 0, 0));
 
-    tft.setCursor(20, 120);
-    tft.print("after about 110 days.");
+    buttons.push_back(Button("cancelScreenOkBtn", 160, 70, 145, 38, "Ok", 60, 8));
+    buttons.push_back(Button("terminateBtn", 160, 130, 145, 28, "End Grow", 30, 7));
 
-    /* Include if you want to know your free memory */
-    //tft.setTextSize(1);
-    //tft.setCursor(50,140);
-    //uint32_t freemem = System.freeMemory();
-    //tft.print(freemem);
-
-    buttons.push_back(Button("cancelScreenOkBtn", 20, 180, 250, 38, "Ok", 110, 8));
-    buttons.push_back(Button("terminateBtn", 120, 10, 185, 28, "Terminate Grow", 10, 7));
     renderButtons(true);
+}
+
+void CancelScreen::drawDay()
+{
+    tft.fillRect(20, 90, 135, 30, ILI9341_BLACK);
+    tft.setCursor(20, 90);
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setTextSize(3);
+    tft.print("Day " + String(tent.state.getDayCount()));
 }
 
 void CancelScreen::renderButton(Button& btn)
@@ -39,11 +37,23 @@ void CancelScreen::renderButton(Button& btn)
 
     } else if (btn.getName() == "cancelScreenOkBtn") {
         drawButton(btn, ILI9341_OLIVE, 3);
+
+    } else if (btn.getName() == "dayUpBtn") {
+        drawButtonTriangleUp(btn, ILI9341_YELLOW);
+
+    } else if (btn.getName() == "dayDownBtn") {
+        drawButtonTriangleDown(btn, ILI9341_YELLOW);
     }
 }
 
 void CancelScreen::renderButtonPressed(Button& btn)
 {
+    if (btn.getName() == "dayUpBtn") {
+        drawButtonTriangleUp(btn, ILI9341_WHITE);
+
+    } else if (btn.getName() == "dayDownBtn") {
+        drawButtonTriangleDown(btn, ILI9341_WHITE);
+    }
 }
 
 void CancelScreen::handleButton(Button& btn)
@@ -53,5 +63,13 @@ void CancelScreen::handleButton(Button& btn)
 
     } else if (btn.getName() == "cancelScreenOkBtn") {
         screenManager.homeScreen();
+
+    } else if (btn.getName() == "dayUpBtn") {
+        tent.state.setDayCount(tent.state.getDayCount() + 1);
+        drawDay();
+
+    } else if (btn.getName() == "dayDownBtn") {
+        tent.state.setDayCount(tent.state.getDayCount() - 1);
+        drawDay();
     }
 }
