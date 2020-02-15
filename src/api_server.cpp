68260f26cd57e2c43fecfa3ac0c86221aafa4c10
Aman Gupta <aman@tmm1.net>
2020-02-15 14:14:11 -0800

    rename all variables to have tomatotent_ prefix

--- src/api_server.cpp
+++ src/api_server.cpp
@@ -37,45 +37,53 @@ void metricsCmd(WebServer& server, WebServer::ConnectionType type, char* url_tai
     server.httpSuccess("text/plain; version=0.0.4; charset=utf-8");
 
     if (tent.rawSensors.tentTemperature != -1) {
-        server << "# HELP tent_temperature The temperature of the tent in Celsius\n";
-        server << "# TYPE tent_temperature gauge\n";
-        server << "tent_temperature " << tent.rawSensors.tentTemperature << "\n";
+        server << "# HELP tomatotent_air_temperature The temperature of the air in Celsius\n";
+        server << "# TYPE tomatotent_air_temperature gauge\n";
+        server << "tomatotent_air_temperature " << tent.rawSensors.tentTemperature << "\n";
     }
 
     if (tent.rawSensors.tentHumidity != -1) {
-        server << "# HELP tent_humidity The relative humidity of the tent\n";
-        server << "# TYPE tent_humidity gauge\n";
-        server << "tent_humidity " << tent.rawSensors.tentHumidity << "\n";
+        server << "# HELP tomatotent_air_humidity The relative humidity of the air\n";
+        server << "# TYPE tomatotent_air_humidity gauge\n";
+        server << "tomatotent_air_humidity " << tent.rawSensors.tentHumidity << "\n";
     }
 
     if (tent.rawSensors.soilTemperature != -1) {
-        server << "# HELP soil_temperature The temperature of the soil in Celsius\n";
-        server << "# TYPE soil_temperature gauge\n";
-        server << "soil_temperature " << tent.rawSensors.soilTemperature << "\n";
+        server << "# HELP tomatotent_soil_temperature The temperature of the soil in Celsius\n";
+        server << "# TYPE tomatotent_soil_temperature gauge\n";
+        server << "tomatotent_soil_temperature " << tent.rawSensors.soilTemperature << "\n";
     }
 
     if (tent.rawSensors.soilMoisture != -1) {
-        server << "# HELP soil_moisture The moisture of the soil in Celsius\n";
-        server << "# TYPE soil_moisture gauge\n";
-        server << "soil_moisture " << tent.rawSensors.soilMoisture << "\n";
+        server << "# HELP tomatotent_soil_moisture The moisture level in the soil\n";
+        server << "# TYPE tomatotent_soil_moisture gauge\n";
+        server << "tomatotent_soil_moisture " << tent.rawSensors.soilMoisture << "\n";
     }
 
     if (tent.state.getDayCount() != -1) {
-        server << "# HELP is_day Is it daylight in the tent?\n";
-        server << "# TYPE is_day gauge\n";
-        server << "is_day " << (tent.state.isDay() ? 1 : 0) << "\n";
+        server << "# HELP tomatotent_grow_days How many days since the grow started?\n";
+        server << "# TYPE tomatotent_grow_days gauge\n";
+        server << "tomatotent_grow_days " << tent.state.getDayCount() << "\n";
 
-        server << "# HELP day_count How many days into the grow?\n";
-        server << "# TYPE day_count gauge\n";
-        server << "day_count " << tent.state.getDayCount() << "\n";
+        server << "# HELP tomatotent_light_on Is it daylight in the tent?\n";
+        server << "# TYPE tomatotent_light_on gauge\n";
+        server << "tomatotent_light_on " << (tent.state.isDay() ? 1 : 0) << "\n"; // TODO expose sunrise/sunset
 
-        server << "# HELP day_duration How many minutes in a day?\n";
-        server << "# TYPE day_duration gauge\n";
-        server << "day_duration " << tent.state.getDayDuration() << "\n";
+        server << "# HELP tomatotent_light_period How many minutes of daylight in the tent?\n";
+        server << "# TYPE tomatotent_light_period gauge\n";
+        server << "tomatotent_light_period " << tent.state.getDayDuration() << "\n";
 
-        server << "# HELP minutes_in_photoperiod How many minutes into the current photoperiod?\n";
-        server << "# TYPE minutes_in_photoperiod gauge\n";
-        server << "minutes_in_photoperiod " << tent.state.getMinutesInPhotoperiod() << "\n";
+        server << "# HELP tomatotent_period_progress How many minutes into the current photoperiod?\n";
+        server << "# TYPE tomatotent_period_progress gauge\n";
+        server << "tomatotent_period_progress " << tent.state.getMinutesInPhotoperiod() << "\n";
+
+        server << "# HELP tomatotent_fan_auto Is the fan running in automatic mode?\n";
+        server << "# TYPE tomatotent_fan_auto gauge\n";
+        server << "tomatotent_fan_auto " << (tent.state.getFanAutoMode() ? 1 : 0) << "\n";
+
+        server << "# HELP tomatotent_fan_speed Speed of the fan\n";
+        server << "# TYPE tomatotent_fan_speed gauge\n";
+        server << "tomatotent_fan_auto " << (tent.state.getFanSpeed() + 5) << "\n";
     }
 }
 
